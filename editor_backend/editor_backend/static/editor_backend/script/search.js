bd4c3f45fa2112b5800e124dfc790f326cff8099
OIex <oleknurvik@outlook.com>
2016-04-25 14:44:58 +0200

    fixes the functionality for search according to how search actually works

--- editor_backend/editor_backend/static/editor_backend/script/search.js
+++ editor_backend/editor_backend/static/editor_backend/script/search.js
@@ -1,29 +1,30 @@
-
-
 var loglevel = 0;
 var searchResultHtml;
 var searchInput;
 var currentSuggestion = "";
-var dummyArticle = {"title":"Article title",
+var completeSearch = false;
+var dummyArticle = {
+    "title": "Article title",
 
-                    "desc":"Praesent sed magna congue, egestas urna eu, posuere est. " +
-                    "Nulla pretium dolor nec velit egestas, sit amet auctor dui tempus.",
+    "desc": "Praesent sed magna congue, egestas urna eu, posuere est. " +
+    "Nulla pretium dolor nec velit egestas, sit amet auctor dui tempus.",
 
-                    "article":"Interdum et malesuada fames ac ante ipsum primis in faucibus." +
-                    " Aenean a mattis nibh, ac luctus augue. Vestibulum eleifend, massa et vehicula " +
-                    "volutpat, nulla sapien malesuada leo, at posuere quam magna et neque. Morbi ut " +
-                    "ante auctor, pretium sem vel, iaculis libero. Pellentesque dictum ipsum ac dolor " +
-                    "consectetur, maximus sodales neque dapibus. Fusce efficitur eros ac nisi auctor, " +
-                    "id rhoncus augue feugiat. Phasellus turpis massa, dictum quis arcu vitae, " +
-                    "ultrices hendrerit dolor. Curabitur dapibus dapibus dui et semper. Nunc dui leo, " +
-                    "suscipit at efficitur eu, consectetur et eros. Phasellus id semper ligula. " +
-                    "Maecenas laoreet lectus in ante congue mattis. Vivamus mattis tortor at odio " +
-                    "varius tincidunt.",
+    "article": "Interdum et malesuada fames ac ante ipsum primis in faucibus." +
+    " Aenean a mattis nibh, ac luctus augue. Vestibulum eleifend, massa et vehicula " +
+    "volutpat, nulla sapien malesuada leo, at posuere quam magna et neque. Morbi ut " +
+    "ante auctor, pretium sem vel, iaculis libero. Pellentesque dictum ipsum ac dolor " +
+    "consectetur, maximus sodales neque dapibus. Fusce efficitur eros ac nisi auctor, " +
+    "id rhoncus augue feugiat. Phasellus turpis massa, dictum quis arcu vitae, " +
+    "ultrices hendrerit dolor. Curabitur dapibus dapibus dui et semper. Nunc dui leo, " +
+    "suscipit at efficitur eu, consectetur et eros. Phasellus id semper ligula. " +
+    "Maecenas laoreet lectus in ante congue mattis. Vivamus mattis tortor at odio " +
+    "varius tincidunt.",
 
-                    "url":"/"};
+    "url": "/"
+};
 
 
-window.onload = function() {
+window.onload = function () {
     init();
 };
 
@@ -38,11 +39,25 @@ function init() {
 function keyboardHandler(e) {
     if (loglevel > 5) console.log("keyboardHandler");
 
-    if (e.keyCode == 40) {replaceSuggestion();}
-    else if (e.keyCode ==13) {console.log("This should force-search");}
-    else {search();}
-
+    switch (e.keyCode) {
+        case 40:
+            replaceSuggestion();
+            break;
+        case 13:
+            search();
+            break;
+        case 32:
+            wordSuggestion();
+            break;
+        case 8:
+            wipeWordSuggestion();
+            break;
+        default:
+            completeSearch = false;
+            break;
+    }
 }
+
 
 /**
  * TODO: Make this do a real search based on the content of searchInput
@@ -50,20 +65,9 @@ function keyboardHandler(e) {
 function search() {
     if (loglevel > 5) console.log("search");
     if (searchInput.value != "") {
-        console.log(searchInput.value + " This is a value")
-        var results = [];
-
-        /** TODO: What does partial really mean?
-         * When do I know if its partial or not?
-         * Can we just get one suggestion at a time ? last completed word probs from me
-         *
-         *
-         */
+        completeSearch = true;
 
         //todo: Send request to search with {'Partial':'true/false', 'Query':'string'}
-        var fakeResultPartial = {'spell': [['jge', 'jeg'], ['er', 'er']]};
-
-        wordSuggestion(fakeResultPartial.spell[fakeResultPartial.spell.length - 1][1], searchInput.value.length);
 
 
         // Splits the search and sends the current word and total search length to
@@ -71,22 +75,31 @@ function search() {
         var searchWords = searchInput.value.split(" ");
         //wordSuggestion(searchWords[searchWords.length-1], searchInput.value.length);
 
-        if (searchWords.length > 2){
-            // TODO: Probs create a function for all this which can be force-called upon enter or search button
-            wipeResults();
-            //TODO: use the URIs to get the real articles
-            results = {'spell': [['jge', 'jeg'], ['er', 'er']], 'results':['URI1', 'URI2', 'URI1' ]}.results;
-            for (var i = 0; i < results.length; i++) {
-                addArticleToResult(results[i]);
-            }
+        // TODO: Probs create a function for all this which can be force-called upon enter or search button
+        wipeResults();
+        //TODO: use the URIs to get the real articles
+
+        var searchResult = {'spell': [['jge', 'jeg'], ['er', 'er']], 'results': ['URI1', 'URI2', 'URI1']};
+        for (var i = 0; i < searchResult.results.length; i++) {
+            addArticleToResult(searchResult.results[i]);
         }
+
+        var suggestion = "";
+        for (var j = 0; j < searchResult.spell.length; j++){
+            suggestion += searchResult.spell[j][1] +" ";
+        }
+        suggestion = suggestion.slice(0, -1);
+        if (suggestion.split(" ") != searchWords) {
+            displayWordSuggestion(suggestion, 0)
+        }
+
     }
     else {
         wipeWordSuggestion();
         wipeResults();
+        completeSearch = false;
     }
 }
-
 
 
 function wipeResults() {
@@ -97,9 +110,20 @@ function wipeResults() {
 
 function wipeWordSuggestion() {
     if (loglevel > 5) console.log("wipeWordSuggestions");
-    document.getElementById("wordSuggestionField").innerHTML ="";
+    document.getElementById("wordSuggestionField").innerHTML = "";
     currentSuggestion = "";
 }
+
+
+function wordSuggestion() {
+    if (loglevel > 5) console.log("Word suggestion");
+    var fakeResultPartial = {'spell': [['jge', 'jeg'], ['er', 'er']]};
+    var tempWord = fakeResultPartial.spell[fakeResultPartial.spell.length - 1][1];
+    var searchLength = searchInput.value.length - tempWord.length;
+
+    displayWordSuggestion(tempWord, searchLength);
+}
+
 
 /**
  * Takes in a word and the current search length and provides a button for
@@ -107,24 +131,30 @@ function wipeWordSuggestion() {
  * @param word
  * @param searchLength
  */
-function wordSuggestion(word, searchLength) {
+function displayWordSuggestion(word, searchLength) {
     if (loglevel > 5) console.log("Word suggestion");
     currentSuggestion = word;
     searchLength = searchLength - word.length;
     var suggestionField = document.getElementById("wordSuggestionField");
     if (word != "") {
-         suggestionField.innerHTML = "<button id='wordSuggestionButton' onclick='replaceSuggestion()'>"
-             + word +" </button>";
+        if (completeSearch) {
+            suggestionField.innerHTML = "<button id='wordSuggestionButton' onclick='replaceSuggestion()'>"
+                + "Mente du: " + word + " </button>";
+        }
+        else {
+            suggestionField.innerHTML = "<button id='wordSuggestionButton' onclick='replaceSuggestion()'>"
+                + word + " </button>";
+        }
         var margin = 0;
 
-        if (searchLength > word.length ) {
-            margin = (8.2 * searchLength) +5;
+        if (searchLength > word.length) {
+            margin = (8.2 * searchLength) + 5;
             if (margin > 700) {
                 margin = 700;  //prevents the suggestions from going out of the screen
             }
         }
         // Aligns the suggestion with where you are in the input field
-        document.getElementById("wordSuggestionButton").style.marginLeft =   margin + "px";
+        document.getElementById("wordSuggestionButton").style.marginLeft = margin + "px";
     }
     else {
         suggestionField.innerHTML = "";
@@ -132,6 +162,9 @@ function wordSuggestion(word, searchLength) {
 }
 
 function replaceSuggestion() {
+    if (completeSearch) {
+        searchInput.value = "";
+    }
     if (loglevel > 5) console.log("replaceSuggestion");
     if (currentSuggestion != "") {
         var searchWords = searchInput.value.split(" ");
@@ -154,7 +187,7 @@ function addArticleToResult(article) {
     if (loglevel > 5) console.log("addArticleToResult");
     searchResultHtml.innerHTML +=
         '<div class="searchResult"> ' +
-        '<a href=" '+ article.url+' "> ' +
+        '<a href=" ' + article.url + ' "> ' +
         '<h3> ' + article.title + ' </h3> ' +
         '<p>' + article.desc + ' </p> ' +
         '</a> ' +
