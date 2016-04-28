04fb90aaf712040739fd4392067947c125af12f5
OIex <oleknurvik@outlook.com>
2016-04-28 12:14:35 +0200

    More json issues...

--- editor_backend/editor_backend/static/editor_backend/script/search.js
+++ editor_backend/editor_backend/static/editor_backend/script/search.js
@@ -51,7 +51,7 @@ function search() {
         var xhtmlSearch = new XMLHttpRequest();
         xhtmlSearch.open("POST", searchServer);
         xhtmlSearch.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhtmlSearch.send(JSON.stringify({'Partial':'false', 'Query': ''+ searchInput.value }));
+        xhtmlSearch.send(JSON.stringify({'Partial':false, 'Query': ''+ searchInput.value }));
         xhtmlSearch.onreadystatechange = function() {
 
             if (xhtmlSearch.readyState == 4 && xhtmlSearch.status == 200) {
@@ -131,7 +131,7 @@ function wordSuggestion() {
     var xhtmlSearch = new XMLHttpRequest();
     xhtmlSearch.open("POST", searchServer);
     xhtmlSearch.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhtmlSearch.send(JSON.stringify({'Partial':'true', 'Query': ''+ searchWord }));
+    xhtmlSearch.send(JSON.stringify({'Partial':true, 'Query': ''+ searchWord }));
     xhtmlSearch.onreadystatechange = function() {
 
         if (xhtmlSearch.readyState == 4 && xhtmlSearch.status == 200) {
