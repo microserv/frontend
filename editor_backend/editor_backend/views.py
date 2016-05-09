88686969358df23758a767c721dc4109f01fb31f
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-05-09 07:51:09 -0400

    Add missing import.

--- editor_backend/editor_backend/views.py
+++ editor_backend/editor_backend/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render
