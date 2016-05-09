b50b7c0a44179d374eb91f29bdb4e25e71c80e29
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-05-09 07:48:18 -0400

    pep8-ify the views file.

--- editor_backend/editor_backend/views.py
+++ editor_backend/editor_backend/views.py
@@ -8,54 +8,65 @@ import requests
 NODE_ADDR = "http://127.0.0.1:9001"
 publish_base_url = "http://despina.128.no/publish"
 
+
 def get_publisher_url():
-	r = requests.get(NODE_ADDR + "/" + "publishing")
-	response_as_json = json.loads(r.text)
-	if response_as_json:
-		return response_as_json
-	else:
-		return None
+    r = requests.get(NODE_ADDR + "/" + "publishing")
+    response_as_json = json.loads(r.text)
+    if response_as_json:
+        return response_as_json
+    else:
+        return None
+
 
 def homepage(request):
-	return render(request, "homepage.html", {});
+    return render(request, "homepage.html", {})
+
 
 def editor(request):
-	return render(request, "editor_page.html", {});
+    return render(request, "editor_page.html", {})
+
 
 def upload_article(request):
-	dict = request.POST.dict()
-	article = {"tags": dict["tags"], "description": dict["description"], "title": dict["title"]}
-	article["article"] = dict["article"].replace("src=\"//www.", "src=\"http://www.")
+    dict = request.POST.dict()
+    article = {"tags": dict["tags"],
+               "description": dict["description"],
+               "title": dict["title"]}
+    article["article"] = dict["article"].replace("src=\"//www.",
+                                                 "src=\"http://www.")
 
-	publisher_url = get_publisher_url()
-	if publisher_url:
-		r = requests.post("http://"+publisher_url+"/save_article", data = article)
-	else:
-		# Do some error handling here.
-		pass
+    publisher_url = get_publisher_url()
+    if publisher_url:
+        r = requests.post("http://" + publisher_url + "/save_article",
+                          data=article)
+    else:
+        # Do some error handling here.
+        pass
 
-	return render(request, "editor_page.html", {});
+    return render(request, "editor_page.html", {})
+
 
 def articles(request):
-        r = requests.get(publish_base_url + "/list")
+    r = requests.get(publish_base_url + "/list")
 
-        if r.url != publish_base_url + "/list":
-                return HttpResponseRedirect(r.url)
+    if r.url != publish_base_url + "/list":
+        return HttpResponseRedirect(r.url)
 
-        json_response = r.json()
-        if r.status_code == 200 and json_response:
-                return render(request, "articles.html", json_response)
-        else:
-                print(r)
+    json_response = r.json()
+    if r.status_code == 200 and json_response:
+        return render(request, "articles.html", json_response)
+    else:
+        print(r)
+
 
 def search(request):
-    return render(request, "search.html", {});
+    return render(request, "search.html", {})
 
 
 def about(request):
-    return render(request, "about.html", {});
+    return render(request, "about.html", {})
+
 
 def article(request):
-	id = request.path[-24:]
-	r = requests.get(publish_base_url + "/article_json/" + id)
-	return render(request, "article.html", r.json())
+    id = request.path[-24:]
+    r = requests.get(publish_base_url + "/article_json/" + id)
+    return render(request, "article.html", r.json())
